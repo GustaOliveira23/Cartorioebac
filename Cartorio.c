6a4f2163deaa204303a502a46e6a7eafb688f5ae
GustaOliveira23 <fgustavooliveira61@gmail.com>
2024-05-27 09:17:04 -0300

    Add files via upload

---
+++ Cartorio.c
@@ -0,0 +1,179 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário
+#include <stdlib.h> //biblioteca de alocação  de espaço em memória
+#include <locale.h> // biblioteca de alocações de tezto por região
+#include <string.h> // biblioteca responsavel pelas strings
+
+int registro() // Função responsável por cadastrar os usuários no sistema
+{
+	//inicio da criação das variaveis/string
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	//termino da criação das variaveis/string
+	
+	printf("Digite o CPF a ser cadastrado: "); // Coletando informações do usuario
+	scanf("%s", cpf); // %s refere-se as strings
+	
+	strcpy(arquivo, cpf); // responsavel por copiar os valores das strings
+	
+	FILE *file; // cria o arquivo
+	file = fopen(arquivo, "w"); // escreve o arquivo
+	file = fopen(arquivo, "a"); // atualiza o arquivo
+	fprintf(file, "CPF: "); // escreve categoria 
+	fprintf(file,cpf); // salva o valor da variavel
+	fclose(file); // fecha o arquivo
+	
+	file = fopen(arquivo, "a");  // atualiza o arquivo
+	fprintf(file, "\nNome: "); // escreve categoria 
+	fclose(file);  // fecha o arquivo
+	
+	printf("Digite o nome a ser cadastrado: "); // Coletando informações do usuario
+	scanf("%s", nome);
+	
+	file = fopen(arquivo, "a");  // atualiza o arquivo
+	fprintf(file,nome);
+	fprintf(file, "\nSobrenome: "); // escreve categoria 
+	fclose(file);  // fecha o arquivo
+	
+	printf("Digite o sobrenome a ser cadastrado: ");   // Coletando informações do usuario
+	scanf("%s", sobrenome);
+	
+	file = fopen(arquivo, "a"); // atualiza o arquivo
+	fprintf(file,sobrenome);
+	fprintf(file, "\nCargo: "); // escreve categoria 
+	fclose(file);   // fecha o arquivo
+	
+	printf("Digite o cargo a ser cadastrado: ");   // Coletando informações do usuario
+	scanf("%s", cargo);
+	
+	file = fopen(arquivo, "a");   // atualiza o arquivo
+	fprintf(file,cargo);
+	fclose(file);   // fecha o arquivo
+}
+
+int consulta()
+{
+	setlocale(LC_ALL, "Portuguese"); // defindo a linguagem
+	
+		//inicio da criação das variaveis/string
+	char cpf[40];
+	char conteudo[200];
+	//termino da criação das variaveis/string
+	
+	printf("Digite o CPF a ser consultado: "); // Coletando informações do usuario
+	scanf("%s",cpf);
+	printf("\nEssas são as informações do usuário: \n");
+	
+	FILE * file;
+	file = fopen(cpf,"r"); //lê o arquivo
+	
+
+	if(file == NULL)
+	{
+		printf("CPF não cadastrado!\n");
+	}
+	
+	while(fgets(conteudo, 200, file) != NULL)
+	{
+	    
+		printf("%s", conteudo);
+		printf("\n\n");
+				
+	}
+	
+	system("pause");
+}
+
+int deletar()
+{
+	setlocale(LC_ALL, "Portuguese"); // defindo a linguagem
+	char cpf[40];
+	
+	printf("Digite o CPF do usuário a ser deletado: ");
+	scanf("%s",cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	fclose(file);
+	
+	if(file == NULL)
+	{
+		printf("O usuario não se encontra no sistema!");
+		system("pause");
+	}
+	else
+	{
+		remove(cpf);
+		printf("Usuário deletado com sucesso!\n");
+	}
+	system("pause");
+	}
+	
+
+int main()
+{
+	int opcao=0; // definindo variaveis
+    int x=1;
+    
+    char Usuario [10]="a";
+    char Senha [10]="b";
+    int comparacao;
+    
+    printf("###Cartório da EBAC###\n\n");
+    printf("Digite seu usuario: ");
+     scanf("%s",Usuario);
+    printf("\nDigite sua senha: ");
+    scanf("%s",Senha);
+
+    comparacao = strcmp(Usuario,"admin");
+    comparacao = strcmp(Senha,"admin");
+    
+    if(comparacao == 0)
+    {
+	
+    
+   		for (x=1;x=1;)// definindo repetição
+		{
+		 system("cls");
+	 
+		setlocale(LC_ALL, "Portuguese"); // defindo a linguagem
+	
+		printf("###Cartório da EBAC###\n\n"); // Inicio do menu
+		printf("\tBem-Vindo!\n\n");
+		printf("Escolha a opção desejada\n\n");
+		printf("\t1 - Registrar nomes\n");
+		printf("\t2 - Consultar nomes\n");
+		printf("\t3 - Deletar nomes\n");
+		printf("Opção: "); //fim do menu
+	
+		scanf("%d", &opcao); // armazenando a escolha do usuario
+	
+		system("cls");
+	
+		switch(opcao) // inicio da seleção
+		{case 1:
+		 registro();
+    	 break;
+     
+		case 2:
+		 consulta();
+		 break;
+		
+		case 3:
+		 deletar();
+		 break;
+ 		
+		default:
+		printf("Opção Invalida!\n");
+		system("pause");	
+		break;} // fim da seleção
+		}
+	}
+	else 
+		printf("Usuario ou senha incorretos!");
+}
+	
+
+
